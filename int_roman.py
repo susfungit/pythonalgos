0fe1e3e17f321f408cdbf7b9b40ce46d6cef3e22
susfungit <sus.sawkar@gmail.com>
2022-08-27 23:03:28 -0400

    modifications

--- int_roman.py
+++ int_roman.py
@@ -22,18 +22,14 @@ XC	90
 CD	400
 CM	900
 """
-class Solution(object):
-	def IntToRoman(self,n):
-		
-		nums=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-		romans=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
-		num=n
-		result=''
-		for i in range(len(nums)):
-			div=num//nums[i]
-			if div:
-				result=result +romans[i]*div
-				num=num%nums[i]	
-		return result
-obj1=Solution()
-print(obj1.IntToRoman(2974))
+def IntToRoman(n):
+	nums=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+	romans=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+	num=n
+	result=''
+	for i in range(len(nums)):
+		div=num//nums[i]
+		if div:
+			result=result +romans[i]*div
+			num=num%nums[i]	
+	return result
